3cb50a7e8f76b32a72e1ee3f4f52f9c7298df6e9
zyaaaaaa939 <ziakiranaagustine@gmail.com>
2025-09-22 05:17:27 +0700

    fix: perbaikan path main.js untuk build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'src', // kasih tau Vite kalau file utama ada di folder src
   server: {
     port: 3000
   },
   build: {
-    outDir: 'dist'
+    outDir: '../dist' // keluarin hasil build di folder dist (di luar src)
   }
 });
